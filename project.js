145e946efa3b1e3691c2e6fbcdd07779cbe25bd3
Yassine <yassinehamdoune55@gmail.com>
2025-10-31 23:15:29 +0100

    Ajout modal pour afficher nom aleatoire

--- project.js
+++ project.js
@@ -4,6 +4,22 @@ function AjoutBtn(){
     const AddList = document.createElement('li')
     list.appendChild(AddList)
     AddList.innerHTML= list1
-
  
+}
+
+function AficheNum(){
+    let listElm = document.querySelectorAll("#list li");
+ 
+    let randomIndex = Math.floor(Math.random() * listElm.length);
+    let randomName = listElm[randomIndex].textContent;
+     
+
+    document.getElementById('randomName').textContent = randomName;
+    document.getElementById('modal').style.display = "block";
+}
+
+
+function closeModal(){
+    document.getElementById('modal').style.display = "none"
+
 }
