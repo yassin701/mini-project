54b046b15e67e8ad5daaf17a6e19c7ecdeedc559
Yassine <yassinehamdoune55@gmail.com>
2025-11-03 09:26:58 +0100

    remove selected random name from list after display

--- project.js
+++ project.js
@@ -1,36 +1,37 @@
 ///////////////////partie ajouter//////////////////////////
 function AjoutBtn(){
- const input = document.getElementById('myInput')
-  const name = input.value.trim()
- const list1 = document.getElementById('list')
- const list = document.createElement('li')
+const input = document.getElementById('myInput')
+const name = input.value.trim()
+const list1 = document.getElementById('list')
+const list = document.createElement('li')
 
-  if(name === ""){
-    alert ("please entre your name !")
+if (name === ""){
+    alert ('please enter your name!')
     return;
-  }
-   
-   list.textContent = name
-   list1.appendChild(list)
-   input.value = "";
 }
-
+      
+list.textContent =name
+list1.appendChild(list)
+input.value = "";
+ 
+}
 
 ////////////////////////// partie affichage ///////////////////////////
 function AficheNum() {
-    const list2 = document.querySelectorAll('#list li') 
-    const num = document.getElementById('randomName')
+const list2 = document.querySelectorAll('#list li')
+const num = document.getElementById('randomName')
 
-    if(list2.length === 0){
-        alert('The list is empty!!')
-        return;
-    }
+  if(list2.length === 0){
+    alert ("The list is empty !!")
+    return;
+  }
+ 
+ const randomIndex =Math.floor(Math.random()* list2.length)
+ const randomList = list2[randomIndex].textContent
+ num.textContent= randomList
+ document.getElementById('modal').style.display = "block"
+ list2[randomIndex].remove();
 
-    const randomIndex = Math.floor(Math.random()* list2.length)
-    const randomName = list2[randomIndex].textContent
-
-    num.textContent = randomName
-    document.getElementById('modal').style.display = "block"
 }
 
 ///////////////close modal/////////////////////
